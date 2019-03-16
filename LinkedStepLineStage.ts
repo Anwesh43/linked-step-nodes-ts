1b820d678e0751b7e1edc86bace14593e4181923
Anwesh43 <anweshthecool0@gmail.com>
2019-03-16 22:27:46 +0530

    adding constants and scale methods

---
+++ LinkedStepLineStage.ts
@@ -0,0 +1,29 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const strokeFactor : number = 90
+const sizeFactor : number = 2.9
+const nodes : number = 5
+const lines : number = 4
+const foreColor : string = "#4CAF50"
+const backColor : string = "#BDBDBD"
+
+const maxScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.max(0, scale - i / n)
+}
+
+const divideScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.min(1 / n, divideScale(scale, i, n)) * n
+}
+
+const scaleFactor : Function = (scale : number) : number => Math.floor(scale / scDiv)
+
+const mirrorValue : Function = (scale : number, a : number, b : number) : number => {
+    const k : number = scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+
+const updateValue : Function = (scale : number, dir : number, a : number, b : number) : number => {
+    return mirrorValue(scale, a, b) * dir * scGap
+}
