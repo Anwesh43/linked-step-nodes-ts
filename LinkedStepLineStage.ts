681cebb723803c06b9efb24227479542d45bc5e3
Anwesh43 <anweshthecool0@gmail.com>
2019-03-16 23:23:37 +0530

    adding StepLine class

--- LinkedStepLineStage.ts
+++ LinkedStepLineStage.ts
@@ -161,3 +161,26 @@ class SLNode {
         return this
     }
 }
+
+class StepLine {
+
+    dir : number = 1
+    curr : SLNode = new SLNode(0)
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
